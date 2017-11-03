ef4921692387571f3dddbc7573f3f0d5c587ef33
Dimon14101010 <dmitriy.nekrasov89>
2017-11-03 14:18:03 +0200

    post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoList = [];
+  todoItem = {
+    id: 0,
+    title: '',
+    description: '',
+    date: 0
+  };
+  constructor (private http: HttpClient) {
+    this.http.get("../assets/base.json")
+      .subscribe((response: Array<any>) => this.todoList = response);
+  }
+  addTodo (title, description) {
+    this.http.post("../assets/base.json" , {
+      id: this.todoList.length + 1,
+      title: title,
+      description: description,
+      date: Date.now()
+    }).subscribe(res => console.log(res));
+    console.log('works', title, description);
+  }
+
 }
