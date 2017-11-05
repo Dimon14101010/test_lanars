7e7c8b5aa4559b636a2150dc2e087b503cd3e7e0
Dimon14101010 <dmitriy.nekrasov89>
2017-11-06 00:31:51 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -12,20 +11,69 @@ export class AppComponent {
     id: 0,
     title: '',
     description: '',
-    date: 0
+    created_at: ''
   };
-  constructor (private http: HttpClient) {
-    this.http.get("../assets/base.json")
-      .subscribe((response: Array<any>) => this.todoList = response);
+  idCounter = 0;
+  sortedArray: Array<any>;
+  itemsOnPage = 10;
+  pages: number;
+  pagesArray: Array<any> = [];
+  startIndex = 0;
+  lastIndex = 10;
+  currerntPage;
+  constructor () {
   }
   addTodo (title, description) {
-    this.http.post("../assets/base.json" , {
-      id: this.todoList.length + 1,
+    this.idCounter++;
+    this.todoItem = {
+      id: this.idCounter,
       title: title,
       description: description,
-      date: Date.now()
-    }).subscribe(res => console.log(res));
-    console.log('works', title, description);
+      created_at: new Date().toLocaleDateString()
+    };
+    this.todoList.push (this.todoItem);
+    this.sortedArray = this.todoList;
+    this.setPage();
+    this.pagination(this.pages);
   }
-
+  removeTodo (index) {
+    this.sortedArray.splice(index, 1);
+    this.setPage();
+    this.pagination(this.pages);
+  }
+  sortByTitle (sortText) {
+    if (sortText) {
+      this.sortedArray = this.todoList.filter(function (sortArr) {
+        if (sortText && sortArr.title.toLowerCase().indexOf(sortText.toLowerCase()) > -1) {
+          return true;
+        }
+      });
+    } else {
+      this.sortedArray = this.todoList;
+    }
+    this.setPage();
+    this.pagination(this.pages);
+  }
+  sortByDate (date) {
+    if (date) {
+      this.sortedArray = this.todoList.filter(function (sortArr) {
+        if (sortArr.created_at === new Date(date).toLocaleDateString()) {
+          return true;
+        }
+      });
+    } else {
+      this.sortedArray = this.todoList;
+    }
+    this.setPage();
+    this.pagination(this.pages);
+  }
+  setPage () {
+    this.pages = Math.ceil(this.sortedArray.length / this.itemsOnPage);
+    this.pagesArray.length = this.pages;
+  }
+  pagination (page) {
+    this.currerntPage = page;
+    this.startIndex = this.itemsOnPage * (page - 1);
+    this.lastIndex = this.itemsOnPage * page;
+  }
 }
